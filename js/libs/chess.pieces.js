f2cc5c20310e8c230376c42b7d95ec2c3fd4b64c
don.c.smith@gmail.com <don.c.smith@gmail.com@1e2581a9-74bf-8781-c4ef-612b7d019cfa>
2012-07-26 20:25:55 +0000

    working on backend agent

--- js/libs/chess.pieces.js
+++ js/libs/chess.pieces.js
@@ -16,7 +16,7 @@ var Piece = Class.extend({
 		this.init_y = yp;
 		this.unmoved = true;
 		this.rep = this._rep[this.type];
-		this.BERep = c.toLowerCase().substring(0, 1) + this._rep[this.type];
+		this.BERep = c.toLowerCase().substring(0, 1) + this._rep[this.type].toLowerCase();
 	},
 	moveIsValid: function(x, y, moves) {
 		for (var i = 0; i < moves.length; i++) {
